5eedb730fd9ec4e68c0c561b474d002961da0034
Fuvid <info@david-amore.de>
2016-12-01 13:11:46 +0100

    added secon challenge

--- src/de/fuvid/adventofcode/day1/Day1.kt
+++ src/de/fuvid/adventofcode/day1/Day1.kt
@@ -1,28 +1,56 @@
-package de.amore.david.adventofcode.day1
+package de.fuvid.adventofcode.day1
 
 /**
  * Created by david.amore on 01.12.16.
  */
-
-fun main(args: Array<String>){
+fun step1(){
     val input ="R1, R3, L2, L5, L2, L1, R3, L4, R2, L2, L4, R2, L1, R1, L2, R3, L1, L4, R2, L5, R3, R4, L1, R2, L1, R3, L4, R5, L4, L5, R5, L3, R2, L3, L3, R1, R3, L4, R2, R5, L4, R1, L1, L1, R5, L2, R1, L2, R188, L5, L3, R5, R1, L2, L4, R3, R5, L3, R3, R45, L4, R4, R72, R2, R3, L1, R1, L1, L1, R192, L1, L1, L1, L4, R1, L2, L5, L3, R5, L3, R3, L4, L3, R1, R4, L2, R2, R3, L5, R3, L1, R1, R4, L2, L3, R1, R3, L4, L3, L4, L2, L2, R1, R3, L5, L1, R4, R2, L4, L1, R3, R3, R1, L5, L2, R4, R4, R2, R1, R5, R5, L4, L1, R5, R3, R4, R5, R3, L1, L2, L4, R1, R4, R5, L2, L3, R4, L4, R2, L2, L4, L2, R5, R1, R4, R3, R5, L4, L4, L5, L5, R3, R4, L1, L3, R2, L2, R1, L3, L5, R5, R5, R3, L4, L2, R4, R5, R1, R4, L3"
-    //val input ="R5, L5, R5, R3"
+
     val commands =input.split(",").map(String::trim)
-    val t : Turtle = Turtle();
+    val t : Turtle = Turtle(false);
     print("Starting at x="+t.position.x+ ", y="+t.position.y+"\n")
     for (command in commands){
         if (command.get(0).equals('L')){
-            println("turn left")
+            //println("turn left")
             t.turnL()
         }else if (command.get(0).equals('R')){
-            println("turn right")
+            // println("turn right")
             t.turnR()
         }
         val steps = command.substring(1).toInt()
-        print("walkin "+steps+" Steps Dir:"+t.looking+"\n")
+        //print("walkin "+steps+" Steps Dir:"+t.looking+"\n")
+        print("("+t.position.x+"|"+t.position.y+") ="+t.looking+""+steps+">")
         t.walk(steps)
-
+        println("("+t.position.x+"|"+t.position.y+")")
     }
     println("\nx="+t.position.x+ ", y="+t.position.y)
     println(t.position.x+t.position.y)
+}
+fun step2(){
+    val input ="R1, R3, L2, L5, L2, L1, R3, L4, R2, L2, L4, R2, L1, R1, L2, R3, L1, L4, R2, L5, R3, R4, L1, R2, L1, R3, L4, R5, L4, L5, R5, L3, R2, L3, L3, R1, R3, L4, R2, R5, L4, R1, L1, L1, R5, L2, R1, L2, R188, L5, L3, R5, R1, L2, L4, R3, R5, L3, R3, R45, L4, R4, R72, R2, R3, L1, R1, L1, L1, R192, L1, L1, L1, L4, R1, L2, L5, L3, R5, L3, R3, L4, L3, R1, R4, L2, R2, R3, L5, R3, L1, R1, R4, L2, L3, R1, R3, L4, L3, L4, L2, L2, R1, R3, L5, L1, R4, R2, L4, L1, R3, R3, R1, L5, L2, R4, R4, R2, R1, R5, R5, L4, L1, R5, R3, R4, R5, R3, L1, L2, L4, R1, R4, R5, L2, L3, R4, L4, R2, L2, L4, L2, R5, R1, R4, R3, R5, L4, L4, L5, L5, R3, R4, L1, L3, R2, L2, R1, L3, L5, R5, R5, R3, L4, L2, R4, R5, R1, R4, L3"
+    //val input ="R8, R4, R4, R8"
+    val commands =input.split(",").map(String::trim)
+    val t : Turtle = Turtle(true);
+    print("Starting at x="+t.position.x+ ", y="+t.position.y+"\n")
+    for (command in commands){
+        if (command.get(0).equals('L')){
+            //println("turn left")
+            t.turnL()
+        }else if (command.get(0).equals('R')){
+            // println("turn right")
+            t.turnR()
+        }
+        val steps = command.substring(1).toInt()
+        //print("walkin "+steps+" Steps Dir:"+t.looking+"\n")
+        print("("+t.position.x+"|"+t.position.y+") ="+t.looking+""+steps+">")
+        t.walk(steps)
+        println("("+t.position.x+"|"+t.position.y+")")
+    }
+    println("\nx="+t.position.x+ ", y="+t.position.y)
+    println(t.position.x+t.position.y)
+}
+
+fun main(args: Array<String>){
+    step1()
+    step2()
 }
--- src/de/fuvid/adventofcode/day1/Turtle.kt
+++ src/de/fuvid/adventofcode/day1/Turtle.kt
@@ -1,6 +1,7 @@
-package de.amore.david.adventofcode.day1
+package de.fuvid.adventofcode.day1
 
-import jdk.nashorn.internal.runtime.ScriptRuntime
+import java.util.*
+
 
 enum class Direction {
     N {
@@ -19,10 +20,10 @@ enum class Direction {
         }
     },S{
         override fun turnR(): Direction {
-            return E
+            return W
         }
         override fun turnL(): Direction {
-            return W
+            return E
         }
     },W{
         override fun turnR(): Direction {
@@ -35,24 +36,43 @@ enum class Direction {
     abstract fun turnR(): Direction
     abstract fun turnL(): Direction
 }
-class Position{
-    var x=0
-    var y=0
+class Position(var x: Int, var y: Int) {
+
     fun go(dir :Direction, steps:Int){
         when(dir){
-            Direction.N -> y+= steps
-            Direction.E -> x+= steps
-            Direction.S -> y-= steps
-            Direction.W -> x-= steps
+            Direction.N -> y += steps
+            Direction.E -> x += steps
+            Direction.S -> y -= steps
+            Direction.W -> x -= steps
         }
     }
+
+    override fun equals(other: Any?): Boolean {
+        when(other){
+            is Position -> return other.x== x&& other.y==y
+        }
+
+        return super.equals(other);
+    }
 }
-class Turtle{
+class Turtle(val visitonce: Boolean) {
     var looking : Direction = Direction.N
+    var position:Position = Position(x= 0, y = 0)
+    var visited : MutableList<Position> = ArrayList()
+    fun walk(steps: Int){
+        for(i in 1..steps){
+            val here = Position(position.x, position.y )
+            if (visited.contains(here)&&visitonce)
+                {
+                    print("stop")
+                    break
+                }else{
+                    visited.add(here)
+                    position.go(looking,1 )
+            }
+        }
+    }
 
-    var position:Position = Position()
-
-    fun walk(steps: Int) = position.go(looking,steps )
     fun turnL(){
         looking = looking.turnL()
     }
